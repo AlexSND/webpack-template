426a50ceeba1a7c987d38045bc0b7dac102b9098
Alexandr Serebrennikov <a.a.serebrennikov.office@gmail.com>
2019-12-20 12:01:51 +0800

    add extracted css

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [MiniCSSExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HTMLPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    }),
+    new MiniCSSExtractPlugin({
+      filename: 'style.css'
+    })
+  ]
 };
